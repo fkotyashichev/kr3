ba4ae94c13c8abb275317a7b02aab3106fd15027
fkotyashichev <fkotyashichev@gmail.com>
2023-08-10 19:56:52 +0300

    add tests

---
+++ func.py
@@ -0,0 +1,23 @@
+import datetime
+import json
+
+
+def load_executed_date(filename):
+    """Функция загрузки выполненных операций из файла"""
+    pass
+
+
+def get_time(date):
+    """ВЫвод даты в нужном формате"""
+    pass
+
+
+def get_from_where_to_where(date):
+    """Вывод Откуда и Куда прошла операция
+    в замаскированном формате"""
+    pass
+
+
+def get_transfer_amount_with_currency(date):
+    """Вывод Суммы перевод и валюты"""
+    pass
